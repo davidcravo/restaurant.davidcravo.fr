d72e3da8ed327bb3785270d35a55599d8e7ae9cd
CRAVO David <104896215+davidcravo@users.noreply.github.com>
2024-12-20 12:54:04 +0100

    Address : ajout de la documentation

--- app/models/Address.php
+++ app/models/Address.php
@@ -2,14 +2,55 @@
 
 namespace App\Models;
 
+/**
+ * Classe représentant une adresse
+ */
 class Address{
 
+    /**
+     * Identifiant unique de l'adresse
+     * 
+     * @var int
+     */
     private int $id;
+
+    /**
+     * Rue et numéro de l'adresse
+     * 
+     * @var string
+     */
     private string $street_address;
+
+    /**
+     * Code postal de l'adresse
+     * 
+     * @var string
+     */
     private string $postal_code;
+
+    /**
+     * Ville de l'adresse
+     * 
+     * @var string
+     */
     private string $city;
+
+    /**
+     * Pays de l'adresse
+     * 
+     * @var string
+     */
     private string $country;
 
+    /**
+     * Constructeur de la classe Address.
+     *
+     * @param int|null $id Identifiant unique de l'adresse (null si non défini).
+     * @param string $street_address Rue et numéro de l'adresse.
+     * @param string $postal_code Code postal de l'adresse.
+     * @param string $city Ville de l'adresse.
+     * @param string $country Pays de l'adresse.
+     */
     public function __construct(?int $id, string $street_address, string $postal_code, string $city, string $country){
         $this->id = $id;
         $this->street_address = $street_address;
@@ -18,6 +59,11 @@ class Address{
         $this->country = $country;
     }
 
+    /**
+     * Récupère toutes les adresses depuis la base de données.
+     *
+     * @return Address[] Liste des adresses sous forme d'objets Address.
+     */
     public static function getAddresses(){
         $pdo = Database::getConnection();
         $stmt = $pdo->query("SELECT * FROM addresses");
