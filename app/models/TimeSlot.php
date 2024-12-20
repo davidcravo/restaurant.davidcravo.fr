a966dceeefe3e5b44d580bb8cd407b5c3386f168
CRAVO David <104896215+davidcravo@users.noreply.github.com>
2024-12-20 16:18:09 +0100

    TimeSlot : ajout de la documentation

--- app/models/TimeSlot.php
+++ app/models/TimeSlot.php
@@ -2,15 +2,66 @@
 
 namespace App\Models;
 
+/**
+ * Classe représentant un créneau horaire.
+ * 
+ * Cette classe permet de gérer les créneaux horaires, de les récupérer depuis
+ * une base de données et de générer leur représentation HTML.
+ */
 class TimeSlot{
 
+    /**
+     * Identifiant unique du créneau horaire.
+     *
+     * @var int
+     */
     private int $id;
+
+    /**
+     * Jour de la semaine associé au créneau horaire.
+     *
+     * @var string
+     */
     private string $day_of_the_week;
+
+    /**
+     * Heure de début de matinée.
+     *
+     * @var string
+     */
     private string $am_start;
+
+     /**
+     * Heure de fin de matinée.
+     *
+     * @var string
+     */
     private string $am_end;
+
+    /**
+     * Heure de début d'après-midi.
+     *
+     * @var string
+     */
     private string $pm_start;
+
+    /**
+     * Heure de fin d'après-midi.
+     *
+     * @var string
+     */
     private string $pm_end;
 
+    /**
+     * Constructeur de la classe TimeSlot.
+     *
+     * @param int $id Identifiant unique du créneau horaire.
+     * @param string $day_of_the_week Jour de la semaine.
+     * @param string|null $am_start Heure de début de matinée (optionnelle).
+     * @param string|null $am_end Heure de fin de matinée (optionnelle).
+     * @param string|null $pm_start Heure de début d'après-midi (optionnelle).
+     * @param string|null $pm_end Heure de fin d'après-midi (optionnelle).
+     */
     public function __construct(int $id, string $day_of_the_week, ?string $am_start, ?string $am_end, ?string $pm_start, ?string $pm_end)
     {
         $this->id = $id;
@@ -21,6 +72,15 @@ class TimeSlot{
         $this->pm_end = $pm_end ?? '';
     }
 
+    /**
+     * Récupère tous les créneaux horaires depuis la base de données.
+     *
+     * Cette méthode exécute une requête SQL pour sélectionner toutes les lignes
+     * de la table `time_slots` et les transforme en instances de `TimeSlot`.
+     *
+     * @return array<TimeSlot> Tableau d'instances de `TimeSlot`.
+     * @throws \PDOException En cas d'erreur lors de l'exécution de la requête SQL.
+     */
     public static function getTimeSlots(): array{
         $pdo = Database::getConnection();
         $stmt = $pdo->query("SELECT * FROM time_slots");
@@ -35,6 +95,14 @@ class TimeSlot{
         ), $results);
     }
 
+     /**
+     * Génère le code HTML pour afficher le créneau horaire.
+     *
+     * Si les horaires du matin ou de l'après-midi sont absents, le créneau est
+     * affiché comme "Fermé".
+     *
+     * @return string Code HTML représentant le créneau horaire.
+     */
     public function toHTML(){
         if(empty($this->am_start) && empty($this->am_end)){
             $am = 'Fermé';
