16e19b7d3417db5507e8cd67e4e16fdcdf077614
CRAVO David <104896215+davidcravo@users.noreply.github.com>
2024-12-20 12:53:30 +0100

    Dish : complétion de la documentation

--- app/models/Dish.php
+++ app/models/Dish.php
@@ -11,63 +11,94 @@ class Dish{
 
     /**
      * Identifiant unique du plat
+     * 
+     * @var int
      */
     private int $id;
 
     /**
      * Chemin vers l'image du plat
+     * 
+     * @var string
      */
     private string $image;
 
     /**
      * Texte alternatif pour l'image du plat
+     * 
+     * @var string
      */
     private string $alternative;
 
     /**
      * Nom du plat
+     * 
+     * @var string
      */
     private string $name;
 
     /**
      * Prix du plat
+     * 
+     * @var float
      */
     private float $price;
 
     /**
      * Catégorie du plat (entrée, plat principal, dessert, etc.)
+     * 
+     * @var string
      */
     private string $course;
 
     /**
      * Lien vers la recette du plat
+     * 
+     * @var string
      */
     private string $link;
 
     /**
      * Description du plat
+     * 
+     * @var string
      */
     private string $description;
 
     /**
      * Répertoire des images des plats
+     * 
+     * @var string
      */
     private string $dirImage = Config::DIR['images'] . 'menu/';
 
     /**
      * Répertoire des vues
+     * 
+     * @var string
      */
     private string $dirView = Config::DIR['views'];
 
     /**
      * Récupère la catégorie du plat
+     * 
+     * @return string Catégorie du plat
      */
     public function getCourse(){
         return $this->course;
     }
 
     /**
-     * Constructeur de la classe Dish
+     * Constructeur de la classe Dish.
+     *
+     * @param int $id Identifiant unique du plat.
+     * @param string $image Nom du fichier image du plat.
+     * @param string $alternative Texte alternatif pour l'image.
+     * @param string $name Nom du plat.
+     * @param float $price Prix du plat.
+     * @param string $course Catégorie du plat (entrée, plat principal, dessert, etc.).
+     * @param string $link Lien vers la recette du plat.
+     * @param string $description Description du plat.
      */
     public function __construct(
         int $id,
