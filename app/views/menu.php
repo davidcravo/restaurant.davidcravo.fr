d8ca773bf395e82428dbfee36eefc41fe9bb5827
CRAVO David <104896215+davidcravo@users.noreply.github.com>
2024-12-21 07:05:34 +0100

    Menu : adaptation à la mise en ligne

--- app/views/menu.php
+++ app/views/menu.php
@@ -5,6 +5,9 @@
     require dirname(__DIR__,2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
     require __DIR__ . DIRECTORY_SEPARATOR . 'partials' . DIRECTORY_SEPARATOR . 'header.php';
 
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'enums' . DIRECTORY_SEPARATOR . 'Course.php';
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . 'Dish.php';
+
     $command = [];
     $courses = Course::getTypeKeysValues();
     $dishes = Dish::getDishes();
