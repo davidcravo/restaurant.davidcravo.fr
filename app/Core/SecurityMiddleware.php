412566d089b03e296421b682d30d21a7792bdc6d
CRAVO David <104896215+davidcravo@users.noreply.github.com>
2025-02-25 14:08:27 +0100

    SecurityMiddleware: ajout d'un enregistrement des tentatives d'accès interdites

--- app/Core/SecurityMiddleware.php
+++ app/Core/SecurityMiddleware.php
@@ -5,6 +5,7 @@ class SecurityMiddleware {
     public static function protect() {
         if (preg_match('/\.(env|log|sql|phpinfo)$/', $_SERVER['REQUEST_URI'])) {
             http_response_code(403);
+            error_log("Tentative d'accès interdit : " . $_SERVER['REQUEST_URI'] . " - " . $_SERVER['REMOTE_ADDR']);
             exit("Accès interdit.");
         }
     }
